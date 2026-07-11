14fd6a39e4aee069680473533196ecda0a147ecb
Anurag Kumar <kumaranurag50534@gmail.com>
2026-07-11 22:00:00 +0530

    feat: Create user domain model

---
+++ taskflow-go/internal/models/user.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	FullName     string    `json:"full_name"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
