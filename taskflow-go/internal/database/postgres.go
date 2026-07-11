5fad495b0734e9c18ea16041e70424357dbf0bc4
Anurag Kumar <kumaranurag50534@gmail.com>
2026-07-11 08:00:00 +0530

    feat: Implement database connection pool

---
+++ taskflow-go/internal/database/postgres.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPostgresPool(ctx context.Context, dbURL string) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse database url: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	log.Println("Connected to PostgreSQL successfully!")
+	return pool, nil
+}
